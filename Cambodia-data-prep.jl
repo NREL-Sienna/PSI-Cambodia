7abbd30e99a53a51f6608a6f60e254d193cf4373
Barrows <clayton.barrows@nrel.gov>
2020-07-11 20:58:56 -0600

    updating pu values

--- Cambodia-data-prep.jl
+++ Cambodia-data-prep.jl
@@ -9,20 +9,20 @@ using DataFrames
 
 
 pownet_data_dir = joinpath("PowNet", "Model")
-siip_data_dir = mkdir("siip_data")
+siip_data_dir = mkpath("siip_data")
 
 branch = CSV.read(joinpath(pownet_data_dir, "data_camb_transparam.csv"))
 gens = CSV.read(joinpath(pownet_data_dir, "data_camb_genparams.csv"))
-loads = CSV.read(joinpath(pownet_data_dir, "data_camb_load_2016.csv"))
-
+loads_ts = CSV.read(joinpath(pownet_data_dir, "data_camb_load_2016.csv"))
 
 branch[!,:r] .= 0.0
 branch[!,:x] .= 0.0
+branch[!,:name] = branch.source .* "_" .* branch.sink
 
 bus = DataFrame(Dict(:node=>union(branch.source, branch.sink)))
 bus[!,:type] .= "PV"
 bus[!,:voltage] .= 100.0
-bus[[b in names(loads) for b in bus.node],:type] .= "PQ"
+bus[[b in names(loads_ts) for b in bus.node],:type] .= "PQ"
 bus[bus.node .== gens[gens.maxcap .== maximum(gens.maxcap),:node], :type] .= "REF"
 bus[!,:id] = [1:nrow(bus)...]
 
@@ -34,10 +34,20 @@ gens[gens.fuel .== "coal", :fuel_price] .= 2.1
 gens[gens.fuel .== "imp", :prime_mover] .="HY"
 gens = gens[gens.fuel .!= "slack",:]
 
+loads = loads_ts[:, [c for c in names(loads_ts) if !(c in ["Year","Month","Day","Hour"])]]
+loads = combine(
+            groupby(
+                stack(loads, variable_name = :node, value_name = :load),
+                :node),
+            :load => maximum)
+loads[!, base_power] =
+
 
 CSV.write(joinpath(siip_data_dir, "bus.csv"), bus)
 CSV.write(joinpath(siip_data_dir, "branch.csv"), branch)
 CSV.write(joinpath(siip_data_dir, "gen.csv"), gens)
+CSV.write(joinpath(siip_data_dir, "load.csv"), loads)
+
 
 
 
